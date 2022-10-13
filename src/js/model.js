38ec8f7d20714f0c2b06fcdd343159cc2e888b18
Sarvesh K <sckargutkar@gmail.comgit config --global user.name Sarvesh>
2022-10-13 18:17:14 +0530

    console

--- src/js/model.js
+++ src/js/model.js
@@ -2,7 +2,7 @@ import { async } from 'regenerator-runtime';
 import { API_URL, RES_PER_PAGE, KEY } from './config.js';
 // import { getJSON, sendJSON } from './helpers.js';
 import { AJAX } from './helpers.js';
-
+console.log('Hello');
 export const state = {
   recipe: {},
   search: {
